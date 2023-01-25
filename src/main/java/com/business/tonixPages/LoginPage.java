6c30ee646039e30f440dc9eef3ea61201f3acc25
Avula Harish <avulaharish1996@gmail.com>
2023-01-25 16:37:24 +0530

    new

--- src/main/java/com/business/tonixPages/LoginPage.java
+++ src/main/java/com/business/tonixPages/LoginPage.java
@@ -8,6 +8,7 @@ public class LoginPage extends BasePage {
     public LoginPage() throws InterruptedException {
         super();
     }
+//hii
     /**
      * Perform login into application
      * Entering password stored in testdata.properties - password field and clicking on login button
