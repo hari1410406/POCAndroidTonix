c1f8a44a5678529755ed37b00685076b409df61c
hari1410406 <103132602+hari1410406@users.noreply.github.com>
2023-01-25 17:55:44 +0530

    Update Utilities.java

--- src/main/java/com/utility/Utilities.java
+++ src/main/java/com/utility/Utilities.java
@@ -136,7 +136,7 @@ public class Utilities extends ExtentReporter {
 
 	public static JavascriptExecutor js;
 
-	public static PropertyFileReader prop = new PropertyFileReader("https://github.com/hari1410406/POCAndroidTonix/tree/main/properties");
+	public static PropertyFileReader prop = new PropertyFileReader(".\\properties\\testdata.properties");
 
 	public void initDriver() {
 		if (getPlatform().equals("Web")) {
